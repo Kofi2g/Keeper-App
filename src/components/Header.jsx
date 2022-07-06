c67a43b822f14c023d2b775765d978a4616e67d5
kofi <sambonful99@hotmail.com>
2022-07-07 00:27:36 +0100

    Completed styling and Adding elements to page using React

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Header() {
+    return <header className="headerH1">
+    <h1>Keeper App</h1>
+    </header>
+}
---
+++ src/components/Note.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Note() {
+   return <div className="note">
+        <h1>Title Here</h1>
+        <p>Content Here</p>
+    </div>
+}
